161b270455b9564b6db9a6f8312edc1f665f8a9a
songjianhua2012 <songjianhua2012@outlook.com>
2015-08-01 17:54:31 +0800

    remove node_modules.

---
+++ spec/module/Pos-spec.js
@@ -0,0 +1,9 @@
+var Pos = require("../../module/Pos.js");
+
+describe("Pos",function() {
+  describe("constructor",function() {
+    it("should show right object",function() {
+
+    });
+  });
+});
